ef98d55b6c35f72142a0b4d22ae43ca5683bada7
your Name <shanzayzafar96@gmail.com>
2019-06-13 11:43:42 +0500

    refactoring

--- src/labTerminalB/Customer.java
+++ src/labTerminalB/Customer.java
@@ -41,7 +41,7 @@ public class Customer {
 		while (rentals.hasMoreElements()) {
 		
 			Rental each = (Rental) rentals.nextElement();
-			double thisAmount = getTotalCharges(each);
+			double thisAmount = each.getTotalCharges();
 			// add frequent renter points
 			frequentRenterPoints += each.getFrequentRentalPoints();
 			//show figures for this rental
@@ -57,24 +57,4 @@ public class Customer {
 		" frequent renter points";
 		return result;
 	}
-		private double getTotalCharges(Rental aRental) {
-			double result = 0;
-			//determine amounts for each line
-			switch (aRental.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				result += 2;
-				if (aRental.getDaysRented() > 2)
-					result += (aRental.getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				result += aRental.getDaysRented() * 3;
-				break;
-			case Movie.CHILDRENS:
-				result += 1.5;
-				if (aRental.getDaysRented() > 3)
-					result += (aRental.getDaysRented() - 3) * 1.5;
-				break;
-			}
-			return result;
-		}
 }
